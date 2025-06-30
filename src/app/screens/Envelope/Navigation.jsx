0f74f5cec323e031dcfedc1cb0c237c93792a0d9
mikewng <156055657+mikewng@users.noreply.github.com>
2025-06-30 19:02:25 -0400

    css changes to nav

--- src/app/screens/Envelope/Navigation.jsx
+++ src/app/screens/Envelope/Navigation.jsx
@@ -9,21 +9,25 @@ const Navigation = () => {
     return (
         <div className={"env-nav-container " + (navState !== "Home" ? "sub-nav" : "main-nav")}>
             <div className="env-imageicon-container" onClick={() => { setNavState("Home") }} />
-            <h1 className="env-header">Cecila</h1>
-            {
-                navState === "Home" &&
-                <div className="env-link-container email">cecilaart@gmail.com</div>
-            }
-            <div className="env-link-container portfolio" onClick={() => { setNavState("Portfolio") }}>
-                Art Portfolio
+            <div className="env-header-container">
+                <div className="env-header">Cecila</div>
+                {
+                    navState === "Home" &&
+                    <div className="env-subheader email">cecilaart@gmail.com</div>
+                }
             </div>
-            <div className="env-link-container projects" onClick={() => { setNavState("Projects") }}>
-                Projects
+            <div className="env-all-links-container">
+                <div className="env-link-container portfolio" onClick={() => { setNavState("Portfolio") }}>
+                    Art Portfolio
+                </div>
+                <div className="env-link-container projects" onClick={() => { setNavState("Projects") }}>
+                    Projects
+                </div>
+                <div className="env-link-container resume" onClick={() => { setNavState("Resume") }}>
+                    Resume
+                </div>
+                <Socials />
             </div>
-            <div className="env-link-container resume" onClick={() => { setNavState("Resume") }}>
-                Resume
-            </div>
-            <Socials />
         </div>
     )
 }
