b67c017b54fcddb9e159642d560aaf315161f68e
mikewng <156055657+mikewng@users.noreply.github.com>
2025-07-16 14:54:38 -0400

    tbi comment

--- src/app/screens/Resume/Resume.jsx
+++ src/app/screens/Resume/Resume.jsx
@@ -6,11 +6,12 @@ const Resume = () => {
     return (
         <div className="resume-container">
             <div className="resume-skills-section">
-                {
+                Skills Word Cloud to be Implemented!
+                {/* {
                     resumeSkillsData.map((skill, i) => {
                         return <ResumeSkill skillMetadata={skill} key={i} />
                     })
-                }
+                } */}
             </div>
             <div className="resume-download-link-section">
                 View Resume
