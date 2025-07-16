f161c532efe53fbaedb2a6ba009d8be37dcf59be
mikewng <156055657+mikewng@users.noreply.github.com>
2025-07-16 01:57:37 -0400

    mroe css styling

--- src/app/screens/Resume/Resume.jsx
+++ src/app/screens/Resume/Resume.jsx
@@ -5,15 +5,15 @@ import ResumeSkill from "./components/ResumeSkill";
 const Resume = () => {
     return (
         <div className="resume-container">
-            <div className="resume-download-link-section">
-                View Resume
-            </div>
             <div className="resume-skills-section">
                 {
                     resumeSkillsData.map((skill, i) => {
-                        return <ResumeSkill skillMetadata={skill} key={i}/>
+                        return <ResumeSkill skillMetadata={skill} key={i} />
                     })
                 }
+            </div>
+            <div className="resume-download-link-section">
+                View Resume
             </div>
         </div>
     )
