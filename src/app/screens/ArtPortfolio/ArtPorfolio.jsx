c96c206d30bcd9ffa051ad5dd03a92a5931428c7
mikewng <156055657+mikewng@users.noreply.github.com>
2025-07-16 00:56:36 -0400

    blur until load

--- src/app/screens/ArtPortfolio/ArtPorfolio.jsx
+++ src/app/screens/ArtPortfolio/ArtPorfolio.jsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState, lazy, Suspense } from "react";
 
-// import ImageGalleryComponent from "./ImageComponent/ImageGalleryComponent";
 import "./ArtPortfolio.scss"
 import "./ImageComponent/ImageGalleryComponent.scss"
 
@@ -13,37 +12,20 @@ const ImageGalleryComponent = lazy(() => import("./ImageComponent/ImageGalleryCo
 
 
 const ArtPortfolio = () => {
-
-    const [isLoadingPictures, setIsLoadingPictures] = useState(true)
     const [displayedImage, setDisplayedImage] = useState(null)
-
-
-    // Add isPinned field to Pictures
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setIsLoadingPictures(false)
-        }, 700);
-
-        return () => clearTimeout(timer);
-    }, []);
 
     const handleOnImgSelect = (image) => {
         setDisplayedImage(image)
     }
 
-
     return (
         <div className="art-gallery-container">
             {
-                !isLoadingPictures ? imgTestArray.map((img, i) => {
+                imgTestArray.map((img, i) => {
                     return (
                         <ImageGalleryComponent imageSource={img} key={i} onSelect={handleOnImgSelect} />
                     )
-                }) :
-                    <div className="art-gallery-loading-container">
-                        <div className="art-gallery-loading-content">Loading...</div>
-                    </div>
+                })
             }
             <ImageViewer
                 isDisplaying={displayedImage}
--- src/app/screens/ArtPortfolio/ImageComponent/ImageGalleryComponent.jsx
+++ src/app/screens/ArtPortfolio/ImageComponent/ImageGalleryComponent.jsx
@@ -1,9 +1,26 @@
 import "./ImageGalleryComponent.scss"
+import { useEffect, useState } from "react";
 
-const ImageGalleryComponent = ({imageSource, onSelect}) => {
+const ImageGalleryComponent = ({ imageSource, onSelect }) => {
+    const [imageSrc, setImageSrc] = useState(imageSource?.blurDataURL);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = imageSource?.src;
+        img.onload = () => {
+            setImageSrc(imageSource?.src);
+            setIsLoading(false);
+        };
+    }, [imageSource?.src]);
+
     return (
-        <div className="img-content-container" onClick={() => {onSelect(imageSource)}}>
-            <img className="img-content-image" src={imageSource?.src} />
+        <div className="img-content-container" style={{ width: imageSource?.width }} onClick={() => { onSelect(imageSource) }}>
+            {
+                isLoading &&
+                <div className="img-content-loading">Loading...</div>
+            }
+            <img className="img-content-image" src={imageSrc} />
         </div>
     )
 }
