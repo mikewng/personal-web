974071b140ecdc66dbd832e7bfb8a3085c3d285c
mikewng <156055657+mikewng@users.noreply.github.com>
2025-07-16 01:08:05 -0400

    css rehaul

--- src/app/screens/ArtPortfolio/ImageViewer/ImageViewer.jsx
+++ src/app/screens/ArtPortfolio/ImageViewer/ImageViewer.jsx
@@ -5,8 +5,8 @@ const ImageViewer = ({ image, isDisplaying, onClose }) => {
         <div className={"img-viewer-container" + (isDisplaying ? " displaying" : "")}>
             <div className="img-viewer-modal-container">
                 <img className="img-viewer-img" src={image?.src} />
-                <div className="img-viewer-close" onClick={() => onClose(true)}></div>
             </div>
+            <div className="img-viewer-close" onClick={() => onClose(true)}></div>
         </div>
     )
 }
