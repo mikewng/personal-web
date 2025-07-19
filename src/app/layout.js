99bfd211d2477999ca52643300c0485480b64996
mikewng <156055657+mikewng@users.noreply.github.com>
2025-07-19 12:29:38 -0400

    added title

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,11 @@
+import pink from "../app/assets/gallery-imgs/pink.jpg"
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Mike Wang</title>
+      </head>
       <body>
         {children}
       </body>
